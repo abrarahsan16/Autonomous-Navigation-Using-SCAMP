41a8b6b6ddb30858e57597cfcf6647b1ae76c751
y289li <64644129+y289li@users.noreply.github.com>
2020-06-19 12:55:31 -0400

    Update camCap.py

--- scamp_ws/src/camCap/Node/camCap.py
+++ scamp_ws/src/camCap/Node/camCap.py
@@ -7,6 +7,7 @@ import rospy
 import cv2
 import os
 import numpy as np
+import pandas as pd
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -16,7 +17,10 @@ from cv_bridge import CvBridge, CvBridgeError
 path = "/home/abrarahsan16/SCAMP/Autonomous-Navigation-Using-SCAMP/scamp_ws/src/camCap/Node/img/"
 bridge = CvBridge()
 
+df=pd.DataFrame({"Time":[0.0],"imageArray":[np.zeros((480,640),dtype=int)]},columns=["Time","imageArray"])#placeholder
+
 def callback(msg):
+    global df
     print("Received")
     try:
         cv2_img = bridge.imgmsg_to_cv2(msg,"bgr8") #Capture the image
@@ -26,9 +30,14 @@ def callback(msg):
     else:
         np_img = np.asarray(cv2_gray) #convert to a numpy array
         time = msg.header.stamp #capture the time on the image
+	seconds=rospy.get_time()
+	
         cv2.imwrite(os.path.join(path,""+str(time)+".jpeg"),np_img)  #save the image. 
 	print("Printed")
-
+	
+	df1=pd.DataFrame({"Time":[seconds],"imageArray":[np_img]},columns=["Time","imageArray"])
+	df=df.append(df1,ignore_index=True)
+	df.to_csv('/home/andrew/turtlebot3_ws/imageArray.csv') #NOTE please change the saving path
 
 def main():
     #Start ROS node
