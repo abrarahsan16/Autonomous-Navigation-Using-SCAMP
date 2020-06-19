9498991c557556deb6ee0a45d4b987b497d14bc9
y289li <64644129+y289li@users.noreply.github.com>
2020-06-18 21:18:17 -0400

    callback function
    every time node receives msgs, the callback function is activated, where the dataframe will be extended over and over again(from line 20 to line 22).

--- scamp_ws/src/Teleop/node/AngularV.py
+++ scamp_ws/src/Teleop/node/AngularV.py
@@ -2,15 +2,26 @@
 
 import rospy
 from sensor_msgs.msg import Imu
+import pandas as pd
 
 df=pd.DataFrame({"Time":[0.0],"AngularV":[0.0]},columns=["Time","AngularV"])
 
 def callback(msg):
 
-	print("Angular velocity is: ",round(msg.angular_velocity.z,3))
+	global df
+	
+	AngularVel=round(msg.angular_velocity.z,3)
+	print("Angular velocity is: ",AngularVel)
+	
 	time=msg.header.stamp
 	print("    at time: ")
-	print(time)
+	seconds=rospy.get_time()
+	print(seconds)
+
+	df1=pd.DataFrame({"Time":[seconds],"AngularV":[AngularVel]},columns=["Time","AngularV"])
+	df=df.append(df1,ignore_index=True)
+	df.to_csv('/home/andrew/turtlebot3_ws/AngularVelocity.csv')# NOTE: please change the saving path  
+
 
 rospy.init_node("AngularVelocity")
 sub=rospy.Subscriber("/imu",Imu,callback)
