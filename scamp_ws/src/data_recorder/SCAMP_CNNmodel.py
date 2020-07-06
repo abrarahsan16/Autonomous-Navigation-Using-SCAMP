04d47a0c79b1c7d0404af4edea49e8d8f34cc596
y289li <64644129+y289li@users.noreply.github.com>
2020-07-06 12:31:43 -0400

    Add files via upload

---
+++ scamp_ws/src/data_recorder/SCAMP_CNNmodel.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+
+
+def CNN(img_width, img_height, img_channels):
+
+	model=Sequential()
+	model.add(Conv2D(32,(4,4),activation="relu",input_shape=(img_width,img_height,img_channels)))
+	model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+	model.add(Conv2D(32,(4,4),activation="relu"))
+	model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+	model.add(Conv2D(32,(4,4),activation="relu"))
+	model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+	model.add(Conv2D(32,(4,4),activation="relu"))
+	model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+	model.add(Flatten())
+	model.add(Dense(200))
+	model.add(Dense(1))
+	model.add(Activation('linear'))
+
+
+	print(model.summary())
+	
+	return model
+
+model=CNN(240,320,1) 
+model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+
+
