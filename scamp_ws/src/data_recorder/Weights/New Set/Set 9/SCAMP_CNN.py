87c7c1ccc04e58ef7414bbfcb6ec71fb5bf5184a
Abrar Ahsan <ahsan.abrar16@hotmail.com>
2020-08-31 20:16:59 -0400

    Modified weights a bit

---
+++ scamp_ws/src/data_recorder/Weights/New Set/Set 9/SCAMP_CNN.py
@@ -0,0 +1,126 @@
+import numpy as np
+import os
+#os.environ['CUDA_VISIBLE_DEVICES']='-1'
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import sys
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
+from keras import optimizers
+
+import utils4 as utils
+import SCAMP_CNNmodel
+
+img_width=480
+img_height=640
+img_channels=1
+output_dim=2
+b_size = 3
+crop_width = 256
+crop_height = 256
+def combine_generator(gen1, gen2):
+    while True:
+        yield(gen1.next(),gen2.next())
+
+model = SCAMP_CNNmodel.CNN(crop_width,crop_height,img_channels,output_dim)
+print("Loaded model")
+
+train_datagen = utils.DataGenerator(rescale = 1./255)
+print("Started Data Collection")
+train_generator = train_datagen.flow_from_directory("/home/abrarahsan16/SCAMP/Autonomous-Navigation-Using-SCAMP/scamp_ws/src/data_recorder/data",
+        shuffle=True, color_mode='grayscale', target_size=(480,256), crop_size=(crop_height,crop_width), batch_size=b_size)
+
+val_generator = train_datagen.flow_from_directory("/home/abrarahsan16/SCAMP/Autonomous-Navigation-Using-SCAMP/scamp_ws/src/data_recorder/test/",
+        shuffle=True, color_mode='grayscale', target_size=(480,256), crop_size=(crop_height,crop_width), batch_size=b_size)
+#xTrain, xTest, yTrain, yTest = train_test_split(train_generator, test_size=0.4, random_state=0)
+
+opt = optimizers.Adam(lr=0.00001)
+
+#model.compile(loss='mean_squared_error', optimizer=opt, metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+print("Training..................................................................")
+
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.i = 0
+        self.x = []
+        self.losses = []
+        self.acc5loss= []
+        self.acc6loss= []
+        self.acc5acc= []
+        self.acc6acc= []
+        self.vallosses = []
+        self.valacc5loss= []
+        self.valacc6loss= []
+        self.valacc5acc= []
+        self.valacc6acc= []
+        self.fig = plt.figure()
+        self.logs = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        # rewrite on_epoch_end, to plt at the end of each epoch,
+        #if things go too off, we dont have to wait until the end to kill it and adjust
+        # I'm not sure if there is a function that can be called every step of training.
+
+        self.logs.append(logs)
+        self.x.append(self.i)
+        self.losses.append(logs.get('loss'))
+        self.acc5loss.append(logs.get('activation_5_loss'))
+        self.acc6loss.append(logs.get('activation_6_loss'))
+        self.acc5acc.append(logs.get('activation_5_acc'))
+        self.acc6acc.append(logs.get('activation_6_acc'))
+        self.valacc5loss.append(logs.get('val_activation_5_loss'))
+        self.valacc6loss.append(logs.get('val_activation_6_loss'))
+        self.valacc5acc.append(logs.get('val_activation_5_acc'))
+        self.valacc6acc.append(logs.get('val_activation_6_acc'))
+        self.vallosses.append(logs.get('val_loss'))
+        #self.acc.append(logs.get('acc'))
+        #self.val_losses.append(logs.get('val_loss'))
+        #self.val_acc.append(logs.get('val_acc'))
+        self.i+= 1
+	#model.save_weights('my_model_weights.h5',overwrite=True) # I didn't define path, so it should be stored in default path. For me
+        # Before plotting ensure at least 2 epochs have passed
+        if len(self.losses) > 1:
+
+            N = np.arange(0, len(self.losses))
+
+            # You can chose the style of your preference
+            # print(plt.style.available) to see the available options
+            #plt.style.use("seaborn")
+
+            # Plot train loss, train acc, val loss and val acc against epochs passed
+            #plt.figure()
+            plt.show(block=False)
+            plt.plot(N, self.losses, label = "train_loss")
+            plt.plot(N, self.vallosses, label = "val_loss")
+
+            #plt.plot(N, self.acc, label = "train_acc")
+            plt.plot(N, self.acc5loss, label = "acc5_loss")
+            plt.plot(N, self.acc6loss, label = "acc6_loss")
+            plt.plot(N, self.acc5acc, label = "acc5_acc")
+            plt.plot(N, self.acc6acc, label = "acc6_acc")
+
+            plt.plot(N, self.valacc5loss, label = "Val_acc5_loss")
+            plt.plot(N, self.valacc6loss, label = "Val_acc6_loss")
+            plt.plot(N, self.valacc5acc, label = "Val_acc5_acc")
+            plt.plot(N, self.valacc6acc, label = "Val_acc6_acc")
+
+            plt.title("Training Loss and Accuracy [Epoch {}]".format(epoch+1))
+            plt.xlabel("Epoch #")
+            plt.ylabel("Loss/Accuracy")
+            plt.legend()
+            #plt.show(block=False)
+            plt.pause(5) # wait for 5 sec and then close the figure so the training can continue.
+            # Make sure there exists a folder called output in the current directory
+            # or replace 'output' with whatever direcory you want to put in the plots
+            plt.savefig('Plot Output/Epoch-{}.png'.format(epoch+1))
+            plt.close()
+            model.save_weights('my_model_weights.h5',overwrite=True) # I didn't define path, so it should be stored in default path. For me it's home/
+
+callbacks = myCallback()
+
+tf.keras.utils.plot_model(model, to_file="model.png",show_layer_names=True,show_shapes=True,rankdir="TB")
+history=model.fit_generator(train_generator,steps_per_epoch=int(train_generator.samples/b_size), validation_data=val_generator,validation_steps=int(val_generator.samples/b_size), max_queue_size=10, epochs=15, verbose=1, callbacks = [callbacks], workers=5)
+#history=model.fit_generator(train_generator,steps_per_epoch=int(train_generator.samples/b_size),max_queue_size=10, epochs=20, verbose=1,callbacks = [callbacks],workers=5)
---
+++ scamp_ws/src/data_recorder/Weights/New Set/Set 9/utils4.py
@@ -0,0 +1,205 @@
+import re
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import numpy as np
+import tensorflow as tf
+import json
+import cv2
+from keras import backend as K
+from keras.preprocessing.image import Iterator
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import load_img
+from keras.utils.generic_utils import Progbar
+from keras.models import model_from_json
+
+class DataGenerator(ImageDataGenerator):
+
+    def flow_from_directory(self, directory, target_size=(480,256),
+            crop_size=(256,256), color_mode='grayscale', batch_size=3,
+            shuffle=True, seed=None, follow_links=False):
+
+        return DirectoryIterator(directory, self, target_size=target_size,
+                crop_size=crop_size, color_mode=color_mode,
+                batch_size=batch_size, shuffle=shuffle,
+                seed=seed, follow_links=follow_links)
+
+class DirectoryIterator(Iterator):
+    """
+    We assume that the folder structure is:
+    root_folder/
+           folder_1/
+                    L/
+                    S/
+                    R/
+
+           folder_2/
+                    L/
+                    S/
+                    R/
+
+           folder_n/
+                    L/
+                    S/
+                    R/
+    """
+
+    def __init__(self, directory, image_data_generator, target_size=(480,256),
+            crop_size=(256,256), color_mode='grayscale',batch_size=32,
+            shuffle=True,seed=None,follow_links=False):
+
+        self.directory = directory
+        self.image_data_generator = image_data_generator
+        self.target_size = tuple(target_size)
+        self.crop_size = tuple(crop_size)
+        self.follow_links = follow_links
+
+        if color_mode not in {'rgb', 'grayscale'}:
+            raise ValueError('Invalid color mode:', color_mode,
+                             '; expected "rgb" or "grayscale".')
+        self.color_mode = color_mode
+        if self.color_mode == 'rgb':
+            self.image_shape = self.crop_size + (3,)
+        else:
+            self.image_shape = self.crop_size + (1,)
+
+        # First count how many experiments are out there
+        self.samples = 0
+
+        experiments = []
+        for subdir in sorted(os.listdir(directory)):
+            if os.path.isdir(os.path.join(directory, subdir)):
+                experiments.append(subdir)
+        self.num_experiments = len(experiments)
+        self.formats = {'png', 'jpeg'}
+
+        # Idea = associate each filename with a corresponding steering angle
+        self.filenames = []
+
+        # Determine the type of experiment (steering) to compute
+        # the loss
+        self.exp_type = []
+
+        for subdir in experiments:
+            subpath = os.path.join(directory, subdir)
+            self._decode_experiment_dir(subpath)
+
+        assert self.samples > 0, "Did not find any data"
+
+        print('Found {} images belonging to {} experiments.'.format(
+                self.samples, self.num_experiments))
+        super(DirectoryIterator, self).__init__(self.samples,
+                batch_size, shuffle, seed)
+
+    def _recursive_list(self, subpath):
+        return sorted(os.walk(subpath, followlinks=self.follow_links),
+                key=lambda tpl: tpl[0])
+
+    def _decode_experiment_dir(self, dir_subpath):
+        exp_type = 1
+
+        # Now fetch all images in the image subdir
+        image_dir_path = os.path.join(dir_subpath, "img2")
+
+        for root, _, files in self._recursive_list(image_dir_path):
+            sorted_files = sorted(files,
+                    key = lambda fname: int(re.search(r'\d+',fname).group()))
+            for frame_number, fname in enumerate(sorted_files):
+                is_valid = False
+                for extension in self.formats:
+                    if fname.lower().endswith('.' + extension):
+                        is_valid = True
+                        break
+                if is_valid:
+                    absolute_path = os.path.join(root, fname)
+                    self.filenames.append(os.path.relpath(absolute_path,
+                            self.directory))
+                    self.exp_type.append(exp_type)
+                    self.samples += 1
+
+    def next(self):
+        with self.lock:
+            index_array = next(self.index_generator)
+        # The transformation of images is not under thread lock
+        # so it can be done in parallel
+        return self._get_batches_of_transformed_samples(index_array)
+
+    def _get_batches_of_transformed_samples(self, index_array) :
+        """
+        Public function to fetch next batch.
+        # Returns
+            The next batch of images and labels.
+        """
+        current_batch_size = index_array.shape[0]
+
+        # Image transformation is not under thread lock, so it can be done in
+        # parallel
+        batch_x = np.zeros((current_batch_size,) + self.image_shape,dtype=K.floatx())
+        batch_steer = np.zeros((current_batch_size, 2,),dtype=K.floatx())
+        batch_coll = np.zeros((current_batch_size, 2,),dtype=K.floatx())
+
+        grayscale = self.color_mode == 'grayscale'
+
+        # Build batch of image data
+        for i, j in enumerate(index_array):
+            fname = self.filenames[j]
+            labelName = fname.split("img2/",1)[1]
+
+            #print(index_array[i])
+
+            #if labelName.startswith("L"):
+            #    batch_steer[i,0:2] = (1,0,0)
+            #elif labelName.startswith("R"):
+            #    batch_steer[i,0:2] = (0,0,1)
+            #else:
+            #    batch_steer[i,0:2] = (0,1,0)
+
+            #print(batch_steer)
+
+            x = cv2.imread(os.path.join(self.directory,fname))
+            x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+
+            #center_width = int(x.shape[1]/2)
+	    #center_height = int(450)
+
+
+            #x = x[center_height - int(self.crop_size[0]):center_height, center_width - int(self.crop_size[1]/2):center_width + int(self.crop_size[1]/2)]
+            x = cv2.resize(x, dsize=(256, 256)) # needs center crop
+
+            x = np.asarray(x, dtype=np.int32)
+            x = x.reshape((x.shape[0],x.shape[1],1))
+
+            batch_x[i] = x
+            cv2.destroyAllWindows()
+
+            # Build batch of steering and collision data
+            #if self.exp_type[index_array[i]] == 1:
+                #print(self.ground_truth[index_array[i]])
+                # Steering experiment (t=1)
+                #batch_steer[i,0:2] = self.ground_truth[index_array[i],0:2]
+            if labelName.startswith("L"):
+                batch_steer[i,0] = 1
+                batch_steer[i,1] = 0
+                batch_coll[i,0] = 1
+                batch_coll[i,1] = 0
+            elif labelName.startswith("R"):
+                batch_steer[i,0] = 0
+                batch_steer[i,1] = 1
+                batch_coll[i,0] = 1
+                batch_coll[i,1] = 0
+            #elif self.exp_type[index_array[i]] == 0:
+            if labelName.startswith("S"):
+                batch_coll[i,0] = 1
+                batch_coll[i,1] = 0
+                batch_steer[i,0] = 1
+                batch_steer[i,1] = 1
+            elif labelName.startswith("B"):
+                batch_coll[i,0] = 0
+                batch_coll[i,1] = 1
+                batch_steer[i,0] = 0
+                batch_steer[i,1] = 0
+
+		#name as LS LB, RS,RB
+
+        batch_y = [batch_steer, batch_coll]
+
+        return batch_x, batch_y
